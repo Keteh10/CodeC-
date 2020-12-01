e4e82de5de1380a21e9ef5bbf4ab5e80b42b28ad
Keteh10 <74253070+Keteh10@users.noreply.github.com>
2020-12-01 15:11:25 +0100

    Add files via upload

---
+++ All_reduce.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpi.h>
+
+#DEFINE SIZE 1
+
+
+int sendto = 2;
+
+int ma_somme_globale(int *sendbuf, int *recvbuf)
+
+int ma_somme_globale(int *sendbuf, int *recvbuf){
+    
+    int sum = 0;
+    
+    for (rank=0){
+        sum = sendbuf;
+    }
+    
+    for (int i=0; i<size; i++)©
+        MPI_Send(sendbuf, SIZE, MPI_INT, i, 0, MPI_COMM_WORLD);
+        sum += sendbuf;
+    }
+    
+    for int i=0; i<size; i++){
+        recvbuf = sum;
+        MPI_Send(recvbuf, SIZE, MPI_INT,0,i,MPI_COMM_WORLD);
+    }
+    return (sum);
+}
+
+int main(int argc, char **argv){
+    
+    int rank, size;
+    int sum1;
+    int tab[10]={1,2,3,4,5,6,7,8,9,10};
+    int tab2[10];
+    
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    
+    sum1 = ma_somme_globale(tab, tab2);
+    MPI_Allreduce(tab, tab2, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
+    MPI_Finalize(); 
+    
+}
